8380f27807c24175aaec163636879687817a99ab
Yutaka Ide <yutaka_ide@itp.co.jp>
2023-12-12 12:52:19 +0900

    fix

---
+++ src/auth_ui/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/auth",
+  webpack: (config, { webpack, isServer, nextRuntime }) => {
+    // Avoid AWS SDK Node.js require issue
+    if (isServer && nextRuntime === "nodejs")
+      config.plugins.push(
+        new webpack.IgnorePlugin({ resourceRegExp: /^aws-crt$/ })
+      );
+    return config;
+  },
+};
+
+module.exports = nextConfig;
