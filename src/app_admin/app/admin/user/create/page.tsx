9bfdc4a25ac5b3c438ff8bd854298d0f69cee074
安本 海斗 <kaito_yasumoto@itp.co.jp>
2023-12-15 16:27:32 +0900

    app_admin: user create

---
+++ src/app_admin/app/admin/user/create/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+import { userCreateForm } from "@/components/common/formActions/userForm";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  userName: z.string().min(2, {
+    message: "userName must be at least 2 characters.",
+  }),
+  userEmail: z.string().min(2, {
+    message: "userName must be at least 2 characters.",
+  }),
+  userCreateDate: z.string().min(2, {
+    message: "userName must be at least 2 characters.",
+  }),
+});
+
+export default function ProfileForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      userName: "",
+      userEmail: "",
+      userCreateDate: "",
+    },
+  });
+
+  return (
+    <div className="mt-[calc(var(--header-height)+2.25rem)] flex flex-col items-center">
+      <div>
+        <span>
+          <b className="text-red-500">*</b>マークのある項目は入力必須項目です。
+        </span>
+        <Form {...form}>
+          <form
+            action={async (FormData) => {
+              await userCreateForm(FormData);
+            }}
+            className="space-y-3"
+          >
+            <FormField
+              control={form.control}
+              name="userName"
+              render={({ field }) => (
+                <FormItem className="max-w-xs w-[100dvw]">
+                  <FormLabel>
+                    userName<b className="text-red-500">*</b>
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="username" required {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="userEmail"
+              render={({ field }) => (
+                <FormItem className="max-w-xs w-[100dvw]">
+                  <FormLabel>
+                    userEmail<b className="text-red-500">*</b>
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="xxx@xxx.email" required {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="userCreateDate"
+              render={({ field }) => (
+                <FormItem className="max-w-xs w-[100dvw]">
+                  <FormLabel>userCreateDate</FormLabel>
+                  <FormControl>
+                    <Input placeholder="last modify" {...field} />
+                  </FormControl>
+                  <FormDescription>last modify </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="float-right">
+              Submit
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
