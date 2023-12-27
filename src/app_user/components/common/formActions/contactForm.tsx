08b49b3836a49cfcb678c0bc07317c637ba33d4c
安本 海斗 <kaito_yasumoto@itp.co.jp>
2023-12-27 17:53:42 +0900

    app_user: add SendEmailCommand

--- src/app_user/components/common/formActions/contactForm.tsx
+++ src/app_user/components/common/formActions/contactForm.tsx
@@ -1,7 +1,53 @@
 "use server";
 import "server-only";
 
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+
 // submit
 export const submitContactForm = async (FormData: FormData) => {
   console.log(FormData);
+
+  run();
+};
+
+const REGION = "ap-northeast-1";
+
+const sesClient = new SESClient({ region: REGION });
+
+const createSendEmailCommand = (toAddress: string, fromAddress: string) => {
+  return new SendEmailCommand({
+    Destination: {
+      CcAddresses: [],
+      ToAddresses: [toAddress],
+    },
+    Message: {
+      // タイトル
+      Subject: {
+        Charset: "UTF-8",
+        Data: "EMAIL_SUBJECT",
+      },
+      // メール本文
+      Body: {
+        Text: {
+          Charset: "UTF-8",
+          Data: "TEXT_FORMAT_BODY",
+        },
+      },
+    },
+    Source: fromAddress,
+  });
+};
+
+const run = async () => {
+  const sendEmailCommand = createSendEmailCommand(
+    String(process.env.SEND_EMAIL_ADDRESS),
+    String(process.env.SEND_EMAIL_ADDRESS)
+  );
+
+  try {
+    return await sesClient.send(sendEmailCommand);
+  } catch (e) {
+    console.error("Failed to send email.");
+    return e;
+  }
 };
---
+++ src/app_user/src/server-utils.ts
@@ -0,0 +1,14 @@
+export const getIdToken = (cookies: any): string => {
+  const keyPrefix = `CognitoIdentityServiceProvider.${process.env.USER_POOL_APP_ID}`;
+  const lastUserKey = `${keyPrefix}.LastAuthUser`;
+
+  const cookieStore = cookies();
+  const tokenUserName = cookieStore.get(lastUserKey)?.value;
+
+  const idToken = cookieStore.get(`${keyPrefix}.${tokenUserName}.idToken`)
+    ?.value;
+
+  console.log(idToken);
+
+  return idToken as string;
+};
