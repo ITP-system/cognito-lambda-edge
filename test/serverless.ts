897dd9a3b043dedf0601331faf36420dc3d01fdf
Yutaka Ide <yutaka_ide@itp.co.jp>
2024-01-02 23:47:35 +0900

    fix

--- test/serverless.ts
+++ test/serverless.ts
@@ -1,17 +1,16 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as CognitoLambdaEdge from '../lib/cognito-lambda-edge-stack';
+// import * as CognitoLambdaEdge from '../lib/serverless-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/cognito-lambda-edge-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CognitoLambdaEdge.CognitoLambdaEdgeStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+// example resource in lib/serverless-stack.ts
+test("SQS Queue Created", () => {
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new CognitoLambdaEdge.CognitoLambdaEdgeStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
 });
